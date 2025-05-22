e6b2e9d4f58fd332360506e30ab25b3eadf429a7
Amadou Wolfgang Cisse <amadou.6e@googlemail.com>
2025-05-22 16:26:18 +0200

    Requirements from file

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ from setuptools import setup, find_packages
 
 curr_file = Path(__file__).parent.resolve()
 
-with Path("README.md").open("r", encoding="utf-8") as fh:
-    long_description = fh.read()
+
+def parse_requirements(filename):
+    print(filename)
+    return [line for line in Path(filename).read_text().splitlines() if line]
+
 
 setup(
     name="py-dockerdb",
@@ -12,7 +15,6 @@ setup(
     author="Amadou Wolfgang Cisse",
     author_email="amadou.6e@googlemail.com",
     description="Python package for working with databases in Docker containers",
-    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/amadou-6e/docker-db.git",
     package_dir={'': 'src'},
@@ -24,18 +26,7 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     python_requires=">=3.6",
-    install_requires=[
-        "psycopg2-binary",
-        "docker",
-        "pytest",
-        "pymongo",
-        "pydantic",
-        "setuptools",
-        "docker",
-        "pyodbc",
-        "mysql-connector-python",
-        "pydos2unix",
-    ],
+    install_requires=parse_requirements("requirements.txt"),
     include_package_data=True,
     package_data={
         "docker_db": ["tests/data/configs/*/*",],
